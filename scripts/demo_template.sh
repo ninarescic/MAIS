5dac4dcbd42665d076bbaeacbea6746ab5f3bc93
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-03-31 22:20:49 +0200

    imgs

---
+++ scripts/demo_template.sh
@@ -0,0 +1,18 @@
+CONFIG=$1
+EXP_ID=$2
+COLUMN=$3
+
+python run_multi_experiment.py -R ../config/random_seeds.txt --n_jobs 4 --n_repeat 100 ${CONFIG} ${EXP_ID}
+
+FILENAMES=""
+LABELS=""
+for FILE in `ls ../data/output/model/history_${EXP_ID}_*.zip`
+do
+    FILENAMES="$FILENAMES $FILE"
+    LABEL=`basename $FILE .zip`
+    LABELS="${LABELS},$LABEL"
+done
+LABELS=`echo $LABELS | sed 's/^,//g' | eval sed 's/history_${EXP_ID}//g'`
+
+python plot_experiments.py $FILENAMES --label_names $LABELS --out_file ${EXP_ID}.png --column ${COLUMN} 
+geeqie ${EXP_ID}.png
---
+++ scripts/verona_sir.sh
@@ -0,0 +1,1 @@
+source demo_template.sh ../config/verona_sir.ini demo_verona_sir I
