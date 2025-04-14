165fe0c716478ea7b40c6577cd16693c672d8113
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-04-14 22:03:27 +0200

    exps are back

---
+++ scripts/example_experiment2.sh
@@ -0,0 +1,26 @@
+if [ -z "$1" ]
+then
+    NJOBS=`nproc --all`
+    NJOBS=`expr $NJOBS / 2`
+else
+    NJOBS=$1
+fi
+echo "Running with $NJOBS processors"
+
+echo "RUN THE SCENARIOS"
+for I in 0 1
+do
+    echo "Running scenario $I"
+    CFG="../config/hodoninsko_exp2_$I.ini"
+    python run_multi_experiment.py -R ../config/random_seeds.txt --n_jobs $NJOBS --n_repeat 1000 $CFG exp2_$I  
+done 
+echo "RUN THE SCENARIOS -- FINISHED."
+
+
+echo "PLOT THE RESULTS"
+FILES="../data/output/model/history_exp2_0.zip"
+FILES="$FILES ../data/output/model/history_exp2_1.zip"
+python plot_experiments.py $FILES --label_names "baseline,no contact restrictions"  --ymax 6500 --out_file exp2_all_infected.png 
+python plot_experiments.py --column I_d $FILES --label_names "baseline,no contact restrictions" --ymax 6500  --out_file exp2_id.png 
+python plot_experiments.py --column D $FILES --label_names "baseline,no contact restrictions" --ymax 180  --out_file exp2_d.png 
+echo "PLOT THE RESULTS -- FINISHED."
